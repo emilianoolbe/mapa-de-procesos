e3b30dd5ce1fb4f5801d972cb3c11f0ad1548156
emilianoolbe <emilianoolbe@gmail.com>
2023-04-17 09:30:21 -0300

    sonner eliminado

--- src/components/crud/Crear.jsx
+++ src/components/crud/Crear.jsx
@@ -1,64 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Icons } from '../../../public/Icons';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { guardadoLocalStorage } from '../../helpers/guardadoLocalStorage';
-import { Toaster } from 'sonner';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Icons } from "../../../public/Icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { guardadoLocalStorage } from "../../helpers/guardadoLocalStorage";
 
-
-export const Crear = ({setEstrategicos, setMisionales, setApoyo, setProcesoEntrada, setSalida, setEvaluacion}) => {
-
-
+export const Crear = ({
+  setEstrategicos,
+  setMisionales,
+  setApoyo,
+  setProcesoEntrada,
+  setSalida,
+  setEvaluacion,
+}) => {
   //Capturo información del formulario
-  const handlerProcesos = e => {
+  const handlerProcesos = (e) => {
     e.preventDefault();
 
     let proceso = {
       id: Date.now(),
       titulo: e.target.titulo.value,
-      proceso: e.target.proceso.value
+      proceso: e.target.proceso.value,
     };
-    
+
     //Guardo información en el estado
-    if (proceso.proceso === 'Proceso Estratégico') {
-
-      setEstrategicos(elementos => {
-        return [...elementos, proceso]
+    if (proceso.proceso === "Proceso Estratégico") {
+      setEstrategicos((elementos) => {
+        return [...elementos, proceso];
       });
-
-    }else if(proceso.proceso === 'Proceso Misional'){
-
-      setMisionales(elementos => {
-        return [...elementos, proceso]
+    } else if (proceso.proceso === "Proceso Misional") {
+      setMisionales((elementos) => {
+        return [...elementos, proceso];
       });
-
-    }else if(proceso.proceso === 'Proceso de Apoyo'){
-      setApoyo(elementos => {
-        return [...elementos, proceso]
+    } else if (proceso.proceso === "Proceso de Apoyo") {
+      setApoyo((elementos) => {
+        return [...elementos, proceso];
       });
-
-    }else if (proceso.proceso === 'Entrada'){
-      setProcesoEntrada(elementos => {
-        return [...elementos, proceso]
+    } else if (proceso.proceso === "Entrada") {
+      setProcesoEntrada((elementos) => {
+        return [...elementos, proceso];
       });
-
-    }else if (proceso.proceso === 'Salida'){
-      setSalida(elementos => {
-        return [...elementos, proceso]
-      }); 
-    }else{
-      setEvaluacion(elementos => {
-        return [...elementos, proceso]
+    } else if (proceso.proceso === "Salida") {
+      setSalida((elementos) => {
+        return [...elementos, proceso];
       });
-    } 
+    } else {
+      setEvaluacion((elementos) => {
+        return [...elementos, proceso];
+      });
+    }
     //Guardo información en localStorage
     guardadoLocalStorage(proceso.proceso, proceso);
-  }
+  };
 
   return (
-    <div className= 'crear-container'>
-      <div className='crear'>
-      <Link  to={`${import.meta.env.VITE_URL}/`}> <Icons icon={faXmark} css='icon-xmark'/></Link>
+    <div className="crear-container">
+      <div className="crear">
+        <Link to={`${import.meta.env.VITE_URL}/`}>
+          {" "}
+          <Icons icon={faXmark} css="icon-xmark" />
+        </Link>
 
         <form onSubmit={handlerProcesos}>
           <select name="proceso">
@@ -70,14 +70,11 @@ export const Crear = ({setEstrategicos, setMisionales, setApoyo, setProcesoEntra
             <option>Salida</option>
           </select>
 
-          <input type="text" name="titulo" placeholder='Título'/>
-          <input type="submit" value="Agregar"  />
-          <div>
-  
-    </div>
+          <input type="text" name="titulo" placeholder="Título" />
+          <input type="submit" value="Agregar" />
+          <div></div>
         </form>
-        <Toaster />
       </div>
-    </div> 
-  )
+    </div>
+  );
 };
